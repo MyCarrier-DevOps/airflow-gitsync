c96c7d7722caed4194e57703329ca655591a6b28
Byran Carlock <bcarlock@mycarrier.io>
2024-05-20 10:13:48 -0500

    Refresh credentials by changing the remote url

--- gitsync.py
+++ gitsync.py
@@ -7,18 +7,19 @@ import sys
 import time
 import datetime
 from github import Auth, Github
-from git import Repo
+from git import Repo, GitCommandError
 from dotenv import load_dotenv
 import gc
 
 OPERATION = sys.argv[1]
 
-if OPERATION == None or OPERATION == 'help':
+if OPERATION is None or OPERATION == 'help':
     print("Please provide a valid operation.")
     print("Valid operations")
     print("'help' for help.")
     print("'sync' for recurring sync.")
     print("'pull' for a one time pull.")
+
 
 class Vault:
     def __init__(self, role_id, secret_id):
@@ -30,6 +31,7 @@ class Vault:
             )
         except Exception as error:
             raise error
+
 
 class GithubSession:
     def __init__(self, role_id, secret_id, organization):
@@ -48,6 +50,7 @@ class GithubSession:
 
     def renew_vault(self):
         self.vault = Vault(role_id=self.role_id, secret_id=self.secret_id)
+
     def pull_secrets(self, organization: str):
         v = self.vault.Client.secrets.kv.v2
         self.pem = v.read_secret_version(mount_point='DevOps', path=f'github/{organization}',
@@ -59,8 +62,11 @@ class GithubSession:
 
     def authenticate(self):
         self.auth = Auth.AppAuth(self.appid, self.pem).get_installation_auth(self.installid)
+
     def connect(self):
         self.session = Github(auth=self.auth)
+
+
 def repo_cleanup(path: str, repos: list[str]):
     directories = [d for d in os.listdir(path)]
     for d in directories:
@@ -68,17 +74,34 @@ def repo_cleanup(path: str, repos: list[str]):
             print(f'{datetime.datetime.now()} Cleaning up {d} from {path}')
             shutil.rmtree(path+'/'+d)
 
+
 def clone(path: str, repos: list[object]):
     for repo in repos:
         Path(path).mkdir(parents=True, exist_ok=True)
         thisPath = Path(f'{path}/git_{repo["repository"]}')
-        if thisPath.is_dir():
-            print(f'{datetime.datetime.now()} Pulling {repo["repository"]} from {repo["org"]} into {path}')
-            thisRepo = Repo(thisPath.__str__())
-            thisRepo.git.pull()
-        else:
-            print(f'{datetime.datetime.now()} Cloning {repo["repository"]} from {repo["org"]} into {path}')
-            Repo.clone_from(f'https://oauth2:{repo["token"]}@github.com/{repo["org"]}/{repo["repository"]}.git', thisPath)
+        auth = Auth.Token(repo["token"])
+        try:
+            if thisPath.is_dir():
+                print(f'{datetime.datetime.now()} Pulling {repo["repository"]} from {repo["org"]} into {path}')
+                thisRepo = Repo(thisPath.__str__())
+                origin = thisRepo.remotes.origin
+                origin.set_url(f'https://oauth2:{repo["token"]}@github.com/{repo["org"]}/{repo["repository"]}.git')
+                try:
+                    origin.pull()
+                except:
+                    print("Unable to pull, removing directory and cloning a fresh copy.")
+                    shutil.rmtree(thisPath)
+                    print(f'{datetime.datetime.now()} Cloning {repo["repository"]} from {repo["org"]} into {path}')
+                    clonePath = f'https://oauth2:{repo["token"]}@github.com/{repo["org"]}/{repo["repository"]}.git'
+                    Repo.clone_from(clonePath, thisPath)
+
+            else:
+                print(f'{datetime.datetime.now()} Cloning {repo["repository"]} from {repo["org"]} into {path}')
+                clonePath = f'https://oauth2:{repo["token"]}@github.com/{repo["org"]}/{repo["repository"]}.git'
+                Repo.clone_from(clonePath, thisPath)
+        except Exception as e:
+            raise e
+
 
 if __name__ == '__main__':
     # Setup vars
@@ -106,16 +129,21 @@ if __name__ == '__main__':
     if not os.path.isdir(DAG_PATH):
         os.makedirs(DAG_PATH)
     loop = True
-    repos = []
     while loop:
+        repos = []
         for ORG_NAME in ORG_NAMES:
             github = GithubSession(ROLE_ID, SECRET_ID, ORG_NAME)
             org = github.session.get_organization(ORG_NAME)
             reposObj = github.session.search_repositories(query=f'org:{ORG_NAME} topic:airflow-dags template:false')
             theseRepos = [{"repository": repo.name, "token": github.auth.token, "org": ORG_NAME} for repo in reposObj]
             repos.extend(theseRepos)
+            try:
+                clone(DAG_PATH, theseRepos)
+            except Exception as e:
+                print(e)
+                github = GithubSession(ROLE_ID, SECRET_ID, ORG_NAME)
+                clone(DAG_PATH, theseRepos)
         repo_cleanup(DAG_PATH, [f"git_{repo['repository']}" for repo in repos])
-        clone(DAG_PATH, repos)
         if OPERATION == 'pull':
                 loop = False
         else:
